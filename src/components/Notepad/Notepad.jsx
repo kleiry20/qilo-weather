d9911fe2ffbc20ed14dd104e64c368acbc34fd5e
kleiry20 <74005559+kleiry20@users.noreply.github.com>
2024-05-31 23:49:08 +0530

    fixing the update logic

--- src/components/Notepad/Notepad.jsx
+++ src/components/Notepad/Notepad.jsx
@@ -3,24 +3,33 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 const Notepad = () => {
-  const [tracks, setTracks] = useState([]);
-  const [currentTrack, setCurrentTrack] = useState(null);
+  const [songList, setSongList] = useState([
+    { id: 1, songname: "Ishq Nachawe" },
+    { id: 2, songname: "Kinni Kinni" },
+    { id: 3, songname: "One Love" },
+    { id: 4, songname: "Dandelions" },
+  ]);
 
-  useEffect(() => {
-    const fetchTracks = async () => {
-      try {
-        const response = await axios.get(
-          "https://theaudiodb.com/api/v1/json/1/searchtrack.php?s=coldplay"
-        );
-        console.log(response);
-        setTracks(response.data.results);
-      } catch (error) {
-        console.error("Error fetching tracks", error);
-      }
-    };
+  const [playlist, setPlaylist] = useState([{ id: 1, songname: "Laado" }]);
+  //   { id: 1, songname: "Laado" }
 
-    // fetchTracks();
-  }, []);
+  function addToPlaylist(id) {
+    // let size = playlist.length;
+    // console.log(size, "size hu main");
+    // const selectedSong = songList.find((id) => id === songList.id);
+    // setPlaylist([...playlist, { id: size + 1, songname: selectedSong }]);
+
+    const selectedSong = songList.find((song) => song.id === id);
+    console.log("sel song", selectedSong);
+    if (selectedSong) {
+      setPlaylist([...playlist, [{ id: 1, songname: selectedSong }]]);
+      setSongList((prevSongList) =>
+        prevSongList.filter((song) => song.id !== id)
+      );
+    }
+
+    console.log("playlist", playlist);
+  }
 
   return (
     <>
@@ -31,51 +40,36 @@ const Notepad = () => {
           <div className="songs">
             <h3>Check out song collection</h3>
             <ul className="list">
-              <div className="list-item">
-                <li>Ishq nachawe</li>
-                <button>del</button>
-                <button>add</button>
-              </div>
-              <div className="list-item">
-                <li>Ishq nachawe</li>
-                <button>del</button>
-                <button>add</button>
-              </div>
-              <div className="list-item">
-                <li>Ishq nachawe</li>
-                <button>del</button>
-                <button>add</button>
-              </div>
-              <div className="list-item">
-                <li>Ishq nachawe</li>
-                <button>del</button>
-                <button>add</button>
-              </div>
+              {songList.map((song) => {
+                return (
+                  <div key={song.id} className="list-item">
+                    <li>{song.songname}</li>
+                    <button>del</button>
+                    <button onClick={() => addToPlaylist(song.id)}>add</button>
+                  </div>
+                );
+              })}
             </ul>
           </div>
           <div className="playlist">
             <h3>Currently listening to</h3>
+            <ul>
+              {playlist &&
+                playlist.map((song) => {
+                  console.log("playlis  when redndeer", playlist);
+                  return (
+                    <div key={song.id} className="list-item">
+                      <li>{song.songname}</li>
+                      <button>del</button>
+                      {/* <button onClick={() => addToPlaylist(song.id)}>
+                        add
+                      </button> */}
+                    </div>
+                  );
+                })}
+            </ul>
           </div>
         </div>
-
-        {/* <div>
-          <h1>Music Player</h1>
-          {tracks.length > 0 && (
-            <ul>
-              {tracks.map((track) => (
-                <li key={track.id} onClick={() => setCurrentTrack(track.audio)}>
-                  {track.name} - {track.artist_name}
-                </li>
-              ))}
-            </ul>
-          )}
-          {currentTrack && (
-            <audio controls autoPlay>
-              <source src={currentTrack} type="audio/mpeg" />
-              Your browser does not support the audio element.
-            </audio>
-          )}
-        </div> */}
       </div>
     </>
   );
